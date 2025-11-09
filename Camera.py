89787a4317dada4a67603e6f36459f70461a3c35
samathgurung-maker <samathgurung@umass.edu>
2025-11-08 19:56:48 -0500

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -1,1 +1,15 @@
+# Camera clicks picture and saves it in jpg or png 
+
+from picamera2 import PiCamera2
+import time 
+
+picam2 = Picamera2()
+
+#warms up the camera
+picam2.start_preview() 
+time.sleep(5)
+
+camera.capture("campic.jpg")
+
+
 
