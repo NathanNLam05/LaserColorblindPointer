74a677434f79d18ede34696168575ce1c179e5f5
samathgurung-maker <samathgurung@umass.edu>
2025-11-08 20:08:48 -0500

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -1,15 +1,30 @@
 # Camera clicks picture and saves it in jpg or png 
 
-from picamera2 import PiCamera2
-import time 
+from picamera2 import Picamera2
+import time
+import os
 
+# Create camera object
 picam2 = Picamera2()
 
-#warms up the camera
-picam2.start_preview() 
-time.sleep(5)
+# Configure camera for still image
+config = picam2.create_still_configuration()
+picam2.configure(config)
 
-picam2.capture("test.jpg")
+# Optional preview (works only if monitor is attached)
+picam2.start_preview()
 
+# Start camera
+picam2.start()
+time.sleep(2)  # give camera time to adjust exposure
 
+# Save image to a folder
+save_path = "/home/pi/Pictures/test.jpg"
+os.makedirs("/home/pi/Pictures", exist_ok=True)
+
+picam2.capture_file(save_path)
+
+print("Image saved to:", save_path)
+
+picam2.stop()
 
